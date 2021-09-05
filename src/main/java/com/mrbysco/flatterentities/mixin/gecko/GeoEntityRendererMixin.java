b474cb1919332e6c47ebcc0c10af78140670c505
Mrbysco <Mrbysco@users.noreply.github.com>
2021-09-05 22:10:43 +0200

    Add support for GeckoLib rendering
    :)

---
+++ src/main/java/com/mrbysco/flatterentities/mixin/gecko/GeoEntityRendererMixin.java
@@ -0,0 +1,60 @@
+package com.mrbysco.flatterentities.mixin.gecko;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mrbysco.flatterentities.Flattener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(software.bernie.geckolib3.renderers.geo.GeoEntityRenderer.class)
+public class GeoEntityRendererMixin<T extends LivingEntity> {
+
+	@Inject(method = "render",
+			locals = LocalCapture.NO_CAPTURE, at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/blaze3d/matrix/MatrixStack;translate(DDD)V",
+			shift = Shift.AFTER,
+			ordinal = 1))
+	public void flatterRender(T entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, CallbackInfo ci) {
+		float f = MathHelper.interpolateAngle(partialTicks, entityIn.prevRenderYawOffset, entityIn.renderYawOffset);
+
+		final boolean shouldSit = entityIn.isPassenger() && (entityIn.getRidingEntity() != null && entityIn.getRidingEntity().shouldRiderSit());
+		if (shouldSit && entityIn.getRidingEntity() instanceof LivingEntity) {
+			float f1 = MathHelper.interpolateAngle(partialTicks, entityIn.prevRotationYawHead, entityIn.rotationYawHead);
+			float f2 = f1 - f;
+			float f3 = MathHelper.wrapDegrees(f2);
+			if (f3 < -85.0F) {
+				f3 = -85.0F;
+			}
+
+			if (f3 >= 85.0F) {
+				f3 = 85.0F;
+			}
+
+			f = f1 - f3;
+			if (f3 * f3 > 2500.0F) {
+				f += f3 * 0.2F;
+			}
+		}
+		double x = entityIn.getPosX();
+		double z = entityIn.getPosZ();
+
+		final PlayerEntity player = Minecraft.getInstance().player;
+		if(player != null) {
+			x -= player.getPosX();
+			z -= player.getPosZ();
+		}
+
+		System.out.println("hey");
+
+		Flattener.prepareFlatRendering(f, x, z, matrixStackIn, entityIn);
+	}
+}
