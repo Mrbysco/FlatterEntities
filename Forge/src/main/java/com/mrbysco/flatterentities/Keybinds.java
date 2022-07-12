9282023b8a31eb5b56ce78f1c26164a880d1e371
Mrbysco <Mrbysco@users.noreply.github.com>
2022-07-13 00:23:22 +0200

    Update forge [push]

--- Forge/src/main/java/com/mrbysco/flatterentities/Keybinds.java
+++ Forge/src/main/java/com/mrbysco/flatterentities/Keybinds.java
@@ -2,21 +2,21 @@ package com.mrbysco.flatterentities;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
 
 public class Keybinds {
-	public static void registerKeybinds(final FMLClientSetupEvent event) {
-		ClientRegistry.registerKeyBinding(FlatKeybinds.KEY_TOGGLE);
+	public static void registerKeybinds(final RegisterKeyMappingsEvent event) {
+		event.register(FlatKeybinds.KEY_TOGGLE);
 	}
 
+
 	@SubscribeEvent
-	public void onKeyInput(KeyInputEvent event) {
+	public void onKeyInput(InputEvent.Key event) {
 		final Minecraft minecraft = Minecraft.getInstance();
-		if(minecraft.screen != null && event.getAction() != GLFW.GLFW_PRESS) return;
+		if (minecraft.screen != null && event.getAction() != GLFW.GLFW_PRESS) return;
 
 		if (InputConstants.isKeyDown(minecraft.getWindow().getWindow(), 292)) return;
 
